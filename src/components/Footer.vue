58b436933e6208ad6fe6ef566c2e683695445b17
alifalfarabi29 <12050112688@students.uin-suska.ac.id>
2022-05-08 21:56:23 +0700

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<template>
+<footer class = "text-white p-3">
+  <div class = "d-flex justify-content-around">
+    <p>&copy; Al-Qur'an Ku</p>
+    <p>Design and Build with &hearts; By Alif Alfarabi.B</p>
+  </div>
+</footer>
+</template>
+
+
+<style scoped>
+footer{
+  font-family: Roboto;
+  background: linear-gradient(45deg,#85586F, #AC7D88);
+  animation: hue-rotate 2s linear infinite alternate;
+  border-color: white;
+}
+@keyframes hue-rotate {
+  to{filter: hue-rotate(90deg);}
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,45 @@
+<script>
+import NavBar from "../components/NavBar.vue";
+import Footer from "../components/Footer.vue";
+export default {
+  name: "AboutView",
+  components: {NavBar,Footer}
+}
+</script>
+
+<template>
+<NavBar/>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <div>
+    <h1 class="p text-center">
+      Al-Quran-Ku was founded in 2022.
+  </h1>
+    <h1 class="text-center">Thank You Everyone, Who Visited My Website &hearts; &hearts;</h1>
+  </div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
